e4530c67e53145805bab52f27826ff5cb7f8d991
Madelyn Leonard <madmadleonard@gmail.com>
2018-03-21 18:42:44 -0500

    Test class that doesn't work

---
+++ test/test_Powerups.js
@@ -0,0 +1,10 @@
+import Powerups from "..prefabs/Powerups.js";
+
+describe("Powerups", function () {
+    let assert = chai.assert;
+
+    it("Can be created", function () {
+        let model = new Powerups();
+        assert.isOk(true);
+    });
+});
